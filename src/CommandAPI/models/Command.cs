d08512d08857c07e674f6e81a91c059f8606144a
khanghv-the-programmer <khanghvse140097@fpt.edu.vn>
2021-02-01 15:52:49 +0700

    Added Command Model to API Project

---
+++ src/CommandAPI/models/Command.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CommandAPI.models
+{
+    public class Command : Controller
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(250)]
+        public string HowTo { get; set; }
+        [Required]
+        public string Platform { get; set; }
+        [Required]
+        public string CommandLine { get; set; }
+    }
+}
